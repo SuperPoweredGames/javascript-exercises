1304bd32c086ed720bab8765bbcb6f92b713c8a0
Craig <craig@superpoweredgames.com>
2024-06-11 17:10:56 +0100

    Completed solution 9, though took more complex approach with online assistance

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+    let removePunctuation = sentence.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+                           .replace(/\s+/g, "").toLowerCase();
 
+    let reversed =  removePunctuation.split("").reverse().join("");
+    
+    if(reversed === removePunctuation){
+        return true;
+    } else {
+        return false;
+    }
+    
 };
 
 // Do not edit below this line
