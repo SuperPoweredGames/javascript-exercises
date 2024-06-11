0628740c5efa63ed952d350cb1b59cd7a17502a9
Craig <craig@superpoweredgames.com>
2024-06-11 15:54:46 +0100

    Completed solution 5, though slight difference in method of solving to solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    if(num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    if(typeof num1 != 'number' || typeof num2 != 'number') {
+        return "ERROR";
+    }
+
+    let result = 0;
+
+    if(num1 > num2) {
+        for(i = num2; i < num1 + 1; i++) {
+            result += i;
+        }
+        
+    } else if (num2 > num1) {
+        for(i = num1; i < num2 + 1; i++) {
+            result += i;
+        }
+
+    } else {
+        return num1;
+    }
+
+    return result;
 
 };
 
