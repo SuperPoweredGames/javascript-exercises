c0dfe4176d8f00176359a5db1c95525e00e6dd1e
Craig <craig@superpoweredgames.com>
2024-06-11 17:24:37 +0100

    Completed solution 10, mostly same method

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    num = parseInt(num);
+
+    if(num < 0) return "OOPS";
+    if(num === 0) return 0;
+
+    let fib = [1,1];
+   
+
+    for(i = 2; i < num; i++) {
+        fib.push(fib[i-1] + fib[i-2]);
+    }
+
+    return fib[num-1];
 };
 
 // Do not edit below this line
