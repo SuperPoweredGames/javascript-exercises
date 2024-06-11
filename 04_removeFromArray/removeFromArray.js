6fabb284638b3d56f7c8def392a720e4c9fe6730
Craig <craig@superpoweredgames.com>
2024-06-11 15:41:36 +0100

    Completes solution 4, though different approach

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayOfNumbers, ...numbersToRemove) {
+    
+let matches = 0;
+let newArray = [];
+
+    for(i = 0; i < arrayOfNumbers.length; i++) {
+            
+        matches = 0;
+
+        for(j = 0; j < numbersToRemove.length; j++) {
+
+            if(arrayOfNumbers[i] === numbersToRemove[j]) {
+                matches++;                
+            }
+        }
+
+        if(matches === 0) {
+            newArray.push(arrayOfNumbers[i]);
+        }
+    }
+    
+
+    return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
