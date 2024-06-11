b7f8728eb60a112fcc079d537588c82a3f1dc428
Craig <craig@superpoweredgames.com>
2024-06-11 11:49:56 +0100

    Completed solutions 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(phraseToRepeat, repeatAmount) {
+    if (repeatAmount < 0) return "ERROR";
+    
+    let newPhrase = '';
 
+    for (i = 0; i < repeatAmount; i++){
+        newPhrase += phraseToRepeat;
+    }
+
+    return newPhrase;
 };
 
 // Do not edit below this line
